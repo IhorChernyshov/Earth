38380d63a34583534e848aade672c1eef61abacc
ihor <igor.chernyshov6636@gmail.com>
2023-08-28 16:21:25 +1000

    slider

--- src/js/files/sliders.js
+++ src/js/files/sliders.js
@@ -142,70 +142,7 @@ function initSlidersScroll() {
 		}
 	}
 }
-window.addEventListener('load', windowLoaded)
-function windowLoaded(){
 
-
-if (document.querySelector('.product__slider')) {
-	new Swiper('.product__slider', {
-		// Подключаем модули слайдера
-		// для конкретного случая
-		// modules: [Pagination, EffectFade, Autoplay],
-
-		// effect: 'fade',
-		// autoplay: {
-		// 	delay: 3000,
-		// 	disableOnInteraction: true,
-		// },
-
-		loop:true,
-            speed:1000,
-            parallax:true,
-            mousewheel:true,
-            keyboard:true,
-		//touchRatio: 0,
-		//simulateTouch: false,
-		// preloadImages: false,
-		// lazy: true,
-		// Dotts
-		// pagination: {
-		// 	el: '.body-main-slider__control',
-		// 	clickable: true,
-		// },
-		// Arrows
-		// navigation: {
-		// 	nextEl: '.about__more .more__item_next',
-		// 	prevEl: '.about__more .more__item_prev',
-		// },
-		/*
-        breakpoints: {
-            320: {
-                slidesPerView: 1,
-                spaceBetween: 0,
-                autoHeight: true,
-            },
-            768: {
-                slidesPerView: 2,
-                spaceBetween: 20,
-            },
-            992: {
-                slidesPerView: 3,
-                spaceBetween: 20,
-            },
-            1268: {
-                slidesPerView: 4,
-                spaceBetween: 30,
-            },
-        },
-        */
-		on: {
-			init: function (){
-                    document.documentElement.classList.add('lod')
-                }
-		}
-	});
-}
-}
 window.addEventListener("load", function (e) {
 	// Запуск инициализации слайдеров
 	initSliders();
